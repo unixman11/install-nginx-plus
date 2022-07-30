154d5805ca115548254a6ae17cd1ca5759b51ea9
unixman11 <unixman11@gmail.com>
2022-07-30 21:32:11 +0700

    update script

---
+++ script_install-nginx-plus/install-nginx-plus.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# ubuntu 20.04 lts work!!!
+
+echo "Cleanup NGINX controller agent."
+
+sudo systemctl stop controller-agent
+
+sudo apt-get purge avrd nginx-plus-module-metrics avrd-libs -y --allow-change-held-packages
+sudo apt-get purge nginx-controller-agent -y --allow-change-held-packages
+sudo apt autoremove -y
+sudo rm -f /etc/apt/sources.list.d/nginx-controller.list
+
+echo "Cleanup old NGINX Plus."
+
+sudo rm -Rf /etc/ssl/nginx
+sudo rm -f /etc/ssl/nginx/nginx-repo.*
+sudo rm -f /etc/apt/sources.list.d/nginx*
+sudo rm -f /etc/apt/apt.conf.d/90pkgs-ngin*
+
+sudo apt-get purge -y --allow-change-held-packages app-protect
+
+sudo apt-get purge -y --allow-change-held-packages nginx-plus-module-*
+
+sudo apt-get purge -y --allow-change-held-packages app-protect-*
+
+sudo apt-get purge -y --allow-change-held-packages nginx-plus*
+
+sudo apt autoremove -y
+
+sudo mkdir /etc/ssl/nginx
+sudo cp nginx-repo.crt /etc/ssl/nginx/
+sudo cp nginx-repo.key /etc/ssl/nginx/
+
+echo "Install the prerequisites packages."
+sudo apt install -y apt-transport-https lsb-release ca-certificates wget gnupg2 ubuntu-keyring
+
+wget -qO - https://cs.nginx.com/static/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+wget -qO - https://cs.nginx.com/static/keys/app-protect-security-updates.key | gpg --dearmor | sudo tee /usr/share/keyrings/app-protect-security-updates.gpg >/dev/null
+
+printf "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://pkgs.nginx.com/plus/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee /etc/apt/sources.list.d/nginx-plus.list
+
+printf "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] https://pkgs.nginx.com/app-protect/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee /etc/apt/sources.list.d/nginx-app-protect.list
+
+printf "deb [signed-by=/usr/share/keyrings/app-protect-security-updates.gpg] https://pkgs.nginx.com/app-protect-security-updates/ubuntu `lsb_release -cs` nginx-plus\n" | sudo tee -a /etc/apt/sources.list.d/nginx-app-protect.list
+
+sudo wget -P /etc/apt/apt.conf.d https://cs.nginx.com/static/files/90pkgs-nginx
+
+sudo apt update
+
+sudo apt install -y nginx-plus=24-3~focal
+
+sudo apt-mark hold nginx-plus
+
+sudo apt install -y app-protect=24+3.583.0-1~focal \
+nginx-plus-module-appprotect=24+3.583.0-1~focal \
+app-protect-plugin=3.583.0-1~focal \
+app-protect-engine=7.7.7-1~focal \
+app-protect-compiler=7.7.7-1~focal
+
+sudo apt-mark hold app-protect nginx-plus-module-appprotect app-protect-plugin app-protect-engine app-protect-compiler
+
+sudo apt install -y nginx-plus-module-njs=24+0.7.0-1~focal
+
+sudo apt-mark hold nginx-plus-module-njs
+
+nginx -v
+
+sudo systemctl enable nginx.service
+
+sudo systemctl restart nginx.service
+
+echo "Install NGINX Plus finished."
